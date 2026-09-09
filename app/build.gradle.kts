4ce685425ad1a0529f0f71790118ce32b43d1b81
brannenbobby-design <brannenbobby@gmail.com>
2026-09-09 06:47:21 -0500

    Bump validated artwork build to v0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.signallost.tv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.3.1"
+        versionCode = 9
+        versionName = "0.3.2"
     }
 
     compileOptions {
