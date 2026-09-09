c42515ebb70bc35d75fd1605392585ae70d671de
brannenbobby-design <brannenbobby@gmail.com>
2026-09-09 06:06:01 -0500

    Bump Signal Lost bedroom interface build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.signallost.tv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.4"
+        versionCode = 6
+        versionName = "0.2.5"
     }
 
     compileOptions {
